db16c2982763c968cc0908466d7e9e3558f45425
Wei Li <west5678@login2.ls5.tacc.utexas.edu>
2017-12-12 20:21:19 -0600

    edit Charged particles

--- Charged_particle/main.cpp
+++ Charged_particle/main.cpp
@@ -2,10 +2,18 @@
 
 using namespace boost::numeric::odeint;
 
+void observer( const state_type &x, const double t){
+	std::cout << t << "\t" << x[0] << " " << x[1] << " " << x[2] << std::endl;
+}
+
 int main(int argc, char** argv){
+	//initialization
+	GRVY::GRVY_Input_Class iparse;	
+	iparse.Open(argv[1]);
+	int n_sizes, dim;
+	iparse.Read_Var("n_sizes", &n_sizes);		
+	iparse.Read_Var("dim", &dim);
 	
-	int n_sizes = argv[2][0] - '0';
-	int dim = argv[3][0] - '0';	
 	double x0[dim];
 	double dt_list[n_sizes];
 	int problem_type;
@@ -16,19 +24,14 @@ int main(int argc, char** argv){
 	parse(argv[1], x0, t1, dt_list, problem_type, solver);
 	
 	//transfer double [] to vector<double>
-	state_type x (x0, x0 + sizeof(x0)/sizeof(double));
 	std::vector<double> time_steps (dt_list, dt_list + sizeof(dt_list)/sizeof(double));
 	
 	//ODE solver
 	if (solver == "RK4"){
 		runge_kutta4< state_type > stepper;
 		for (auto dt : time_steps){
-			integrate_const(stepper, trajectory, x, 0.0, t1, dt);
-			for (std::vector<double>::const_iterator i = x.begin(); 
-			i != x.end(); i++){
-				std::cout << *i << " ";
-			}
-			std::cout << std::endl;
+			state_type x (x0, x0 + sizeof(x0)/sizeof(double));
+			integrate_const(stepper, trajectory, x, 0.0, t1, dt, observer);
 		}
 	}else if (solver == "RK5"){
 		runge_kutta_dopri5< state_type > stepper;
--- Charged_particle/parse.cpp
+++ Charged_particle/parse.cpp
@@ -13,16 +13,16 @@ void parse(char* filename, double* x, double& t1, double* dt_list,
 		exit(1);
 	
 	//Read single-valued inputs
-	iparse.Read_Var("Problem", &problem_type, 1);
-	iparse.Read_Var("Solver", &solver, "RK4");
-	iparse.Read_Var("t1", &t1, 1.024);
+	iparse.Read_Var("Problem", &problem_type);
+	iparse.Read_Var("Solver", &solver);
+	iparse.Read_Var("t1", &t1);
 	
 	//Read vectors
 	int n_sizes;
-	iparse.Read_Var("n_sizes", &n_sizes, 9);
+	iparse.Read_Var("n_sizes", &n_sizes);
 	iparse.Read_Var_Vec("dt", dt_list, n_sizes);
 
 	int dim;
-	iparse.Read_Var("dim", &dim, 6);
+	iparse.Read_Var("dim", &dim);
 	iparse.Read_Var_Vec("x0", x, dim);
 }
